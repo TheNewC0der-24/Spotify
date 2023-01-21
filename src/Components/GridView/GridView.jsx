302b9adcd4a0d52389cc8f201c57c65dafc6b734
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-21 14:06:49 +0530

    Create Dummy UI

--- src/Components/GridView/GridView.jsx
+++ src/Components/GridView/GridView.jsx
@@ -4,6 +4,9 @@ import {
     Container,
     Grid,
     Card,
+    CardActionArea,
+    CardMedia,
+    Typography,
     CardContent,
 } from '@mui/material';
 
@@ -14,9 +17,18 @@ const GridView = () => {
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6} md={4} lg={4}>
                         <Card sx={{ bgcolor: "#f5f5f5", height: "100%" }}>
-                            <CardContent>
-
-                            </CardContent>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    image="https://i.scdn.co/image/ab67616d00001e021431c3bdf16aa99f71799d95"
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Track Name
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
                         </Card>
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={4}>
